cbe144d0ce075174cb6f524f1b2a672fe5a16926
VahturovaEll <vahturova.e@gmail.com>
2021-02-04 01:38:09 +0400

    создание функций генерации случайных чисел вхазаддых диапазонах

---
+++ js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const getRandomInteger = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  if (max <= min) {
+    max = min + 1;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+getRandomInteger();
+
+const getRandomNumber = function (min, max, decimal_places) {
+  if (max <= min) {
+    max = min + 1;
+  }
+
+  return (Math.random() * (max - min) + min).toFixed(decimal_places);
+};
+
+getRandomNumber();
