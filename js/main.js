566b3516da24902911570944571676d60f651d2f
VahturovaEll <vahturova.e@gmail.com>
2021-02-05 16:06:42 +0400

    правки по заданию. Доработка условий функций

--- js/main.js
+++ js/main.js
@@ -4,20 +4,30 @@ const getRandomInteger = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  if (max <= min) {
-    max = min + 1;
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-getRandomInteger();
-
-const getRandomNumber = function (min, max, decimal_places) {
-  if (max <= min) {
-    max = min + 1;
+  if (max < min) {
+    [min, max] = [max, min];
   }
 
-  return (Math.random() * (max - min) + min).toFixed(decimal_places);
+  if (min >= 0 && max >= 1) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  throw new Error('Введено значение вне диапазона');
+
 };
 
-getRandomNumber();
+getRandomInteger(0, 10);
+
+const getRandomNumber = function (min, max, decimalPlaces) {
+  if (max < min) {
+    [min, max] = [max, min];
+  }
+
+  if (min >= 0 && max >= 1) {
+    return (Math.random() * (max - min) + min).toFixed(decimalPlaces);
+  }
+
+  throw new Error('Введено значение вне диапазона');
+};
+
+getRandomNumber(0, 10);
