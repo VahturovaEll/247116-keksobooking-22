d4f5a498b87681b2b7366939bbfdebd58a9c3eaf
VahturovaEll <vahturova.e@gmail.com>
2021-02-25 14:06:37 +0400

    Начало 6,2 и правка 5,2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-import {adverts} from './data.js';
+//import {adverts} from './data.js';
 
-import {createCard} from './card.js';
+//import {createCard} from './card.js';
 
+import './data.js';
+import './card.js';
 import './form.js';
 import './map.js';
